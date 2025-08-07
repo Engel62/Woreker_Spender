407438024d6d926594f48cf7d09b6875ee865dc2
Engel62 <sr.engel@bk.ru>
2025-08-07 10:38:40 +0300

    1

---
+++ src/AbstractClient.java
@@ -0,0 +1,30 @@
+public abstract class AbstractClient implements Client, Runnable {
+    protected String name;
+    protected int money;
+    protected volatile boolean isRunning;
+
+    public AbstractClient(String name, int initialMoney) {
+        this.name = name;
+        this.money = initialMoney;
+        this.isRunning = true;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getMoney() {
+        return money;
+    }
+
+    @Override
+    public void setMoney(int amount) {
+        this.money = amount;
+    }
+
+    public void stop() {
+        isRunning = false;
+    }
+}
---
+++ src/Client.java
@@ -0,0 +1,5 @@
+public interface Client {
+    String getName();
+    int getMoney();
+    void setMoney(int amount);
+}
